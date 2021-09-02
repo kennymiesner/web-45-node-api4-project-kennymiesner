59d1726c6f4701c9515fc97aec4c09a25f0092c2
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-02 15:14:53 -0500

    Adds express and cors

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const server = require('express')()
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
 
 const port = 5000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Basic API Scaffolding</h1>`)
+})
+
+server.get('/api', (req, res) => {
+  res.json({ message: 'Basic GET Request Test' })
+})
 
 server.listen(port, () => {
   console.log(`\n *** Server Listening on Port ${port} *** \n`)
