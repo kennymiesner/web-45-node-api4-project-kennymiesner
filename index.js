ed532049f4d3bec51eff4429193ef3091bc154a5
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-02 15:20:06 -0500

    Adds prod port and deployability

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const server = express()
 
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 server.use(express.json())
 server.use(cors())
@@ -16,6 +16,6 @@ server.get('/api', (req, res) => {
   res.json({ message: 'Basic GET Request Test' })
 })
 
-server.listen(port, () => {
-  console.log(`\n *** Server Listening on Port ${port} *** \n`)
+server.listen(PORT, () => {
+  console.log(`\n *** Server Listening on Port ${PORT} *** \n`)
 })
