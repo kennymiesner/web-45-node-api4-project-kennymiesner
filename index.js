c9d3b36eff28eb1e61e8908f07ee5158e3fea208
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-02 16:29:39 -0500

    Refactor index.js

--- index.js
+++ index.js
@@ -9,12 +9,19 @@ const PORT = process.env.PORT || 5000
 server.use(express.json())
 server.use(cors())
 
-server.get('/', (req, res) => {
+server.get('/api', (req, res) => {
+  res.json({ message: 'Basic GET Request Test' })
+})
+
+server.use('*', (req, res) => {
   res.send(`<h1>Basic API Scaffolding Project</h1>`)
 })
 
-server.get('/api', (req, res) => {
-  res.json({ message: 'Basic GET Request Test' })
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  })
 })
 
 server.listen(PORT, () => {
